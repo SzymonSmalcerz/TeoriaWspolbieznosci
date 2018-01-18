07d5e09c0bb48e6f9cf1c3261e574ddf55fa0df2
Szymon Smalcerz <smalcerzszymonn@gmail.com>
2018-01-18 21:17:11 +0100

    added matrix generator

--- TW_PROJEKT/src/main/java/com/smalcerz/Test.java
+++ TW_PROJEKT/src/main/java/com/smalcerz/Test.java
@@ -3,18 +3,45 @@ package com.smalcerz;
 public class Test {
 	public static void main(String[] args) {
 		
-		int n = 10;
+		
+		
+		//printRounded(-24000.123, 23123.1233, 10);
+		printFloats(-24000.123, 23123.1233, 10);
+	}
+	
+	
+	public static void printRounded(double min, double max, int n) {
+		System.out.println(n);
 		for(int i=0;i<n + 1;i++) {
 			for(int j=0;j<n;j++) {
 				if(j == (n-1)) {
-					System.out.print(Math.round(Math.random() * 899 + 100));
+					System.out.print(Math.round(Math.random() * (max-min) + min));
 				}else {
-					System.out.print(Math.round(Math.random() * 899 + 100) + " ");
+					System.out.print(Math.round(Math.random() * (max-min) + min) + " ");
 				}
 			}
-			System.out.println();
+			
+			if(i!= n) {
+				System.out.println();
+			}
 		}
 	}
 	
+	public static void printFloats(double min, double max, int n) {
+		System.out.println(n);
+		for(int i=0;i<n + 1;i++) {
+			for(int j=0;j<n;j++) {
+				if(j == (n-1)) {
+					System.out.print(Math.random() * (max-min) + min);
+				}else {
+					System.out.print(Math.random() * (max-min) + min + " ");
+				}
+			}
+			
+			if(i!= n) {
+				System.out.println();
+			}
+		}
+	}
 	
 }
